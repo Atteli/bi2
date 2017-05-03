7b4057d9febc8ae5bf1bc01db9a81b238abf5665
Alexandru Mautner <vasile-alexandru.mautner@student.uni-tuebingen.de>
2017-05-03 13:37:12 +0200

    globals adjustment

--- assignment1/nussinov.py
+++ assignment1/nussinov.py
@@ -1,35 +1,42 @@
 import numpy as np
 
 
-def deltafunc(seq, i, j, gc, au, gu):
-    test = ord(seq[i]) + ord(seq[j])
-    return au if test == 150 else gc if test == 138 else gu if test == 156 else 0
+__GC = 0
+__AU = 0
+__GU = 0
+__MIN_LOOP = 0
+__M = []
+__SEQ = ''
 
-def nussinovfill(seq:str, gc=2, au=3, gu=1, min_loop=0):
-    m = np.zeros((len(seq), len(seq)))
-    for l in range(1, len(seq)):
-        for j in range(l, len(seq)):
+def deltafunc(i, j):
+    test = ord(__SEQ[i]) + ord(__SEQ[j])
+    return __GC if test == 138 else __AU if test == 150 else __GU if test == 156 else 0
+
+def nussinovfill():
+    m = np.zeros((len(__SEQ), len(__SEQ)))
+    for l in range(1, len(__SEQ)):
+        for j in range(l, len(__SEQ)):
             i = j - l
             m[i, j] = np.max([m[i + 1, j],
                               m[i, j - 1],
-                              m[i + 1, j - 1] + deltafunc(seq, i, j, gc, au, gu) if min_loop < j - i else 0,
+                              m[i + 1, j - 1] + deltafunc(i, j) if __MIN_LOOP < j - i else 0,
                               np.max(0 if j - i < 2 else [m[i, k] + m[k + 1, j] for k in range(i + 1, j)])])
     return m
 
-def nussinovtraceback(seq, m, i, j, gc, au, gu):
+def nussinovtraceback(i, j):
     if i < j:
-        if m[i, j] == m[i + 1, j]:
-            nussinovtraceback(seq, m, i + 1, j, gc, au, gu)
-        elif m[i,j] == m[i, j-1]:
-            nussinovtraceback(seq, m, i, j - 1, gc, au, gu)
-        elif m[i,j] == m[i + 1, j - 1] + deltafunc(seq, i, j, gc, au, gu):
-            print (i + 1, seq[i], seq[j], j + 1)
-            nussinovtraceback(seq, m, i + 1, j - 1, gc, au, gu)
+        if __M[i, j] == __M[i + 1, j]:
+            nussinovtraceback(i + 1, j)
+        elif __M[i,j] == __M[i, j-1]:
+            nussinovtraceback(i, j - 1)
+        elif __M[i,j] == __M[i + 1, j - 1] + deltafunc(i, j):
+            print (i + 1, __SEQ[i], __SEQ[j], j + 1)
+            nussinovtraceback(i + 1, j - 1)
         else:
             for k in range (i + 1, j - 1):
-                if m[i, j] == m[i, k] + m[k + 1, j]:
-                    nussinovtraceback(seq, m, i, k, gc, au, gu)
-                    nussinovtraceback(seq, m, k + 1, j, gc, au, gu)
+                if __M[i, j] == __M[i, k] + __M[k + 1, j]:
+                    nussinovtraceback(i, k)
+                    nussinovtraceback(k + 1, j)
                     break
 
     return 0
@@ -38,13 +45,13 @@ if __name__ == '__main__':
     seq1 = 'GUUCAUAAGAGGUCAACAGCAACGGGUGUU'
     seq2 = 'GGGAAACCU'
 
-    seq = seq1
-    gc = 1
-    au = 10
-    gu = 0
-    min_loop = 5
+    __SEQ = seq1
+    __GC = 1
+    __AU = 10
+    __GU = 0
+    __MIN_LOOP = 5
 
-    m = nussinovfill(seq, gc, au, gu, min_loop)
-    print(m)
-    nussinovtraceback(seq, m, 0, len(seq) - 1, gc, au, gu)
-    print('Score:', m[0, len(m[0]) - 1])
+    __M = nussinovfill()
+    print(__M)
+    nussinovtraceback(0, len(__SEQ) - 1)
+    print('Score:', __M[0, len(__M[0]) - 1])
