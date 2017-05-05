f8272d364cde2b0bfe81f8c9a9b91b33c73e2e6d
Alexandru Mautner <atteli@freenet.de>
2017-05-06 00:35:45 +0200

    whatever

--- assignment1/nussinov.py
+++ assignment1/nussinov.py
@@ -60,24 +60,43 @@ def nussinovtraceback(i, j):
                     nussinovtraceback(k + 1, j)
                     break
 
-    return 0
-
-def resultformat(mode='bpseq'):
+def resultformat(mode='bpseq', verbose=False):
     if __SEQ and __PAIRS:
         temp = list(__SEQ)
         for pair in __PAIRS:
             if mode == 'bpseq':
-                print(pair[0] + 1, __SEQ[pair[0]], __SEQ[pair[1]], pair[1] + 1)
+                if verbose:
+                    print(pair[0] + 1, __SEQ[pair[0]], __SEQ[pair[1]], pair[1] + 1)
             temp[pair[0]] = '('
             temp[pair[1]] = ')'
         if mode == 'dot-bracket':
             for i in range(len(temp)):
                 if not (temp[i] == '(' or temp[i] == ')'):
                     temp[i] = '.'
-            print(''.join(temp))
+            if verbose:
+                print(''.join(temp))
+            return ''.join(temp)
+        return __PAIRS
 
-if __name__ == '__main__':
+def printresults(verbose=False):
+    if verbose:
+        print('\nSequence:', __SEQ)
+        print('\n')
 
+        print('bpseq:\n')
+    bpseq = resultformat(mode='bpseq')
+
+    if verbose:
+        print('\ndot-bracket:\n')
+    db = resultformat(mode='dot-bracket')
+
+    if verbose:
+        print('\n')
+        print('Score:', __M[0, len(__M[0]) - 1])
+
+    return bpseq, db
+
+def onconsole():
     parser = argparse.ArgumentParser(description="Nussinov algorithm")
     parser.add_argument("--fasta", help="Query sequence fasta")
     parser.add_argument("--min_loop", nargs='?', help="Minimum loop length, default: 3", type=int, default=3)
@@ -88,32 +107,96 @@ if __name__ == '__main__':
     args = parser.parse_args()
     print(args)
     if args.fasta is None:
-        print('Usage: python3 nussinov.py --fasta {fastafile} [--min_loop {int}] [--GC {int}] [--AU {int}] [--GU {int}]')
+        print(
+            'Usage: python3 nussinov.py --fasta {fastafile} [--min_loop {int}] [--GC {int}] [--AU {int}] [--GU {int}]')
         sys.exit('Error! No FASTA file specified. Exiting...')
-
 
     headers, seqs = fastaread(args.fasta)
 
-    #print(headers, seqs)
+    global __SEQ
+    global __GC
+    global __AU
+    global __GU
+    global __MIN_LOOP
 
-    __SEQ = seqs[0]
+    __SEQ = seqs[0].upper().replace('T', 'U')
     __GC = args.GC
     __AU = args.AU
     __GU = args.GU
     __MIN_LOOP = args.min_loop
 
-    print('\nSequence:', __SEQ)
-    print('\n')
+def onide(gc, au, gu, min_loop):
+    headers, seqs = fastaread('E:/GitProjects/bi2/assignment1/supplement/at.fasta')
+
+    global __SEQ
+    global __GC
+    global __AU
+    global __GU
+    global __MIN_LOOP
+
+    __SEQ = seqs[0].upper().replace('T', 'U')
+    __GC = gc
+    __AU = au
+    __GU = gu
+    __MIN_LOOP = min_loop
+
+def resetglobals():
+    global __SEQ
+    global __GC
+    global __AU
+    global __GU
+    global __MIN_LOOP
+    global __M
+    global __PAIRS
+
+    __GC = 0
+    __AU = 0
+    __GU = 0
+    __MIN_LOOP = 0
+    __M = []
+    __SEQ = ''
+    __PAIRS = []
+
+def bruteforce(r):
+    global __M
+    cmpseq = '(((((((..((((.......)))).(((((.......))))).....((.((.......)).))))))))).'
+    tmp = 0
+    tmpdb = ''
+    tmpscr = 0
+    tmpargs = []
+    for gc in range(r):
+        for au in range(r):
+            for gu in range(r):
+                for min_loop in range(5,10):
+                    resetglobals()
+                    onide(gc, au, gu, min_loop)
+                    __M = nussinovfill()
+                    nussinovtraceback(0, len(__SEQ) - 1)
+                    bpseq, db = printresults()
+                    print(db, tmp / (r*r*r), __GC, __AU, __GU, __MIN_LOOP, __M[0, len(__M[0])-1])
+                    if __M[0, len(__M[0])-1] > tmpscr:
+                        tmpdb = db
+                        tmpscr = __M[0, len(__M[0])-1]
+                        tmpargs = [gc, au, gu, min_loop]
+                    tmp += 1
+                    if db == cmpseq:
+                        print('found config!', __GC, __AU, __GU, __MIN_LOOP)
+                        sys.exit()
+    print('finished.')
+    print(tmpdb, tmpscr, tmpargs)
+
+if __name__ == '__main__':
+    '''
+    #onconsole()
+    onide()
 
     __M = nussinovfill()
     nussinovtraceback(0, len(__SEQ) - 1)
 
-    print('bpseq:\n')
-    resultformat(mode='bpseq')
+    bpseq, db = printresults()
+    '''
+    bruteforce(10)
 
-    print('\ndot-bracket:\n')
-    resultformat(mode='dot-bracket')
 
-    print('\n')
-    print('Score:', __M[0, len(__M[0]) - 1])
+
 
