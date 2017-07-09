744e63f8511e30f6e4b78d7fba90efb9827d1150
Alexandru Mautner <atteli@freenet.de>
2017-07-09 17:42:49 +0200

    ass4 task1 finished

--- assignment4/pepident.py
+++ assignment4/pepident.py
@@ -1,13 +1,72 @@
 import argparse
 import sys
 import pandas as pd
+import numpy as np
 
-import assignment3.peptidemasshandler as ph
+#identification of sequence
+def ident_seq(spectrum, aa_table, ch_m_y, ch_m_b, acc):
+    actual_b_mass = 0
+    actual_y_mass = 0
+    seq_b = []
+    seq_y = []
+    aa_table = aa_table.sort_values(by=['monoisotopic'], ascending=[True])
+    aa_table.reset_index(drop=True, inplace=True)
+
+    #go over spectrum values
+    for ind1, mass in enumerate(spectrum):
+        b_candidates = []
+        y_candidates = []
+        actual_b_am = 0
+        actual_y_am = 0
+        actual_b_offset = 1
+        actual_y_offset = 1
+
+        #test if an amino acid fits to spectrum value
+        for ind2, am in enumerate(aa_table.monoisotopic):
+            b_offset = np.abs((mass - ch_m_b - actual_b_mass - am))
+            y_offset = np.abs((mass - ch_m_y - actual_y_mass - am))
+
+            if b_offset < acc:
+                b_candidates.append(aa_table.code[ind2])
+                actual_b_am = am
+                actual_b_offset = b_offset
+            elif y_offset < acc:
+                y_candidates.append(aa_table.code[ind2])
+                actual_y_am = am
+                actual_y_offset = y_offset
+            else:
+                continue
+
+        if len(b_candidates) and actual_b_offset < actual_y_offset:
+            actual_b_mass += actual_b_am
+            seq_b.append(b_candidates)
+        if len(y_candidates) and actual_y_offset < actual_b_offset:
+            actual_y_mass += actual_y_am
+            seq_y.append(y_candidates)
+
+    return seq_b, seq_y
+
+#tool to print out the sequence as string instead of list
+def pretty_print(lst):
+    result = []
+    for item in lst:
+        if len(item) > 1:
+            result.extend('[')
+            for ind, elem in enumerate(item):
+                result.extend(elem)
+                if ind < len(item) - 1:
+                    result.extend(',')
+            result.extend(']')
+        else:
+            result.extend(item)
+
+    return ''.join(result)
 
 if __name__ == "__main__":
+    #arg parsing
     parser = argparse.ArgumentParser(description="MS experiment")
     parser.add_argument("--msspectrum", help="Tandem MS spectrum of an unknown peptide")
-    parser.add_argument("--masses", nargs='?', help="Masses of amino acids", type=str, default='../assignment3/aa_masses_extended.csv')
+    parser.add_argument("--masses", nargs='?', help="Masses of amino acids", type=str, default='./aa_masses.csv')
 
     args = parser.parse_args()
 
@@ -15,12 +74,22 @@ if __name__ == "__main__":
         print('Usage: python3 pepident.py --msspectrum {tandem ms spectrum file}')
         sys.exit('Error! No spectrum file specified. Exiting...')
 
-    spectrum = args.msspectrum
-    ms = args.masses
+    #file parsing and data preparation
+    spectrumfile = list(open(args.msspectrum))
+    aa_table = pd.read_csv(args.masses, sep=',')
+    spectrum = np.array([spectrumfile[i].strip('\n') for i in range(len(spectrumfile))], dtype=np.float32)
+    spectrum.sort()
 
-    table = pd.read_csv(ms, sep=',')
-    table = table.set_index("monoisotopic")
+    #mh+ charge
+    ch_m_b = 1.00728
+    ch_m_y = (3 * 1.00728) + 15.9949146
 
-    ch_m = (3 * 1.00728) + 15.9949146
+    #mass accuracy (Da); the absolute of +- 0.05 Da is 0.1 Da.
+    acc = 0.1
 
-    print(spectrum, table, ch_m)
+    #result output
+    bseq, yseq = ident_seq(spectrum, aa_table, ch_m_y, ch_m_b, acc)
+    print('sequence (b ion construction):', pretty_print(bseq))
+    print('sequence (y ion construction):', pretty_print(yseq))
+
+
